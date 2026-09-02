b7316668ced93f9baf9dca83ee5a9b61e875de76
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-09-02 16:09:07 +0500

    feat(compose.yaml): add whisper service

--- internal/whisper/transcriber.go
+++ internal/whisper/transcriber.go
@@ -1,6 +1,8 @@
 package whisper
 
 import (
+	"bytes"
+	"mime/multipart"
 	"os"
 
 	"github.com/gazizvr/whisper-bot/internal/ffmpeg"
@@ -25,5 +27,15 @@ func (t *Transcriber) ToSrt(
 	mediaPath string,
 	language string,
 ) (*os.File, error) {
+	wavPath, err := t.convertor.ToWav(mediaPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var byteWriter bytes.Buffer
+	writer := multipart.NewWriter(&byteWriter)
+	writer.WriteField("response_format", "srt")
+	writer.Close()
+
 	return nil, nil
 }
